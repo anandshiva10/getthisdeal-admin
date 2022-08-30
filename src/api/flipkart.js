4c7502ea4687777fb933d9ad727fcc97cd750596
anandshiva10 <anandshiva10@gmail.com>
2022-08-30 23:23:46 +0530

    admin changes

---
+++ src/api/flipkart.js
@@ -0,0 +1,6 @@
+import client from "flipkart-api-affiliate-client/index";
+
+export var fkClient = new client({
+    trackingId:"anjisince",
+    token:"c619b706ba7e4716ba82a1581574c498",
+},'json');
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -8,6 +8,14 @@ import { ClimbingBoxLoader, ScaleLoader } from 'react-spinners'
 const Table = ({setLoader,loader,setLoading}) => {
     const [data,setData] = useState({})
     const [dataFound, setDataFound]=useState(false)
+    const [sortKey,setSortKey]=useState("latest")
+    const initialValues={
+        total:"0",
+        amazon:"0",
+        flipkart:"0",
+        others:"0"
+    }
+    const [count,setCount]=useState(initialValues)
     useEffect(() => {
         const productListRef= collection(firestore,'products')
         
@@ -16,7 +24,41 @@ const Table = ({setLoader,loader,setLoading}) => {
                 data: doc.data(),
                 id: doc.id,
             }))
-            setData(products)
+            // const sproduct=products.sort(function(a,b){
+            //     return b.data.created_timeStamp-a.data.created_timeStamp
+            //   })
+            //   setData(sproduct)
+            if(products.length>=1){
+                var sproduct=[]
+                if(sortKey.includes('oldest')){
+                    sproduct =products.sort(function(a,b){
+                        return a.data.created_timeStamp-b.data.created_timeStamp
+                      })
+                    }
+                else if(sortKey.includes('latest')){
+                     sproduct=products.sort(function(a,b){
+                        return b.data.created_timeStamp-a.data.created_timeStamp
+                      })
+                }
+                else if(sortKey.includes('highest')){
+                    sproduct=products.sort(function(a,b){
+                       return b.data.app_sale_price-a.data.app_sale_price
+                     })
+               }
+               else if(sortKey.includes('lowest')){
+                sproduct=products.sort(function(a,b){
+                   return a.data.app_sale_price-b.data.app_sale_price
+                 })
+           }
+           else if(sortKey.includes('discount')){
+            sproduct=products.sort(function(a,b){
+               return b.data.discount_percentage-a.data.discount_percentage
+             })
+       }   
+                      setData(sproduct)
+                      console.log(data)
+                
+              }
             setDataFound(true)
             console.log(data)
         }).catch(error =>console.log(error));
@@ -42,13 +84,87 @@ const Table = ({setLoader,loader,setLoading}) => {
                 toast.error(err)
             }
             setDataFound(false)
+            
             setLoader(!loader)
             
         }
     }
+    
+    useEffect(() => {
+        console.log(sortKey)
+        setDataFound(false)
+      setLoader(!loader)
+    }, [sortKey])
+    
+    useEffect(() => {
+        if(data.length>0){
+      setCount({...count,})
+            var ama=0
+            var fk=0
+            var ot=0
+            data.forEach(function(a){
+                console.log(a.data.site)
+                if(a.data.site==='amazon')
+                ama=ama+1
+                else if(a.data.site==='flipkart')
+                fk=fk+1
+                else
+                ot=ot+1
+            })
+            setCount({...count,total:data.length,amazon:ama,flipkart:fk,others:ot}) 
+        }
+        console.log(ama)
+    }, [data])
+    
 
   return (
+    <div>
+        <div className='badges'>
+            <div className='badge'>
+                <div className='label'>
+                    Total Products
+                </div>
+                <div className='value'>
+                   {count.total}
+                </div>
+            </div>
+            <div className='badge'>
+                <div className='label'>
+                    Amazon
+                </div>
+                <div className='value'>
+                    {count.amazon}
+                </div>
+            </div>
+            <div className='badge'>
+                <div className='label'>
+                    Flipkart
+                </div>
+                <div className='value'>
+                    {count.flipkart}
+                </div>
+            </div>
+            <div className='badge'>
+                <div className='label'>
+                   Others
+                </div>
+                <div className='value'>
+                {count.others}
+                </div>
+            </div>
+ 
+        </div>
     <div className='productList'>
+            <div className='sorter'>
+            Sort by:
+            <select value={sortKey} name="sort" onChange={e=>setSortKey(e.target.value)}>
+            <option value="latest">Latest products</option>
+            <option value="oldest">Oldest products</option>
+            <option value="lowest">lowest price</option>
+            <option value="highest">highest price</option>
+            <option value="discount">Discount %</option>
+          </select>
+            </div>
           {
                 dataFound? data.length>=1?
         <table className='styled-table'>
@@ -60,7 +176,8 @@ const Table = ({setLoader,loader,setLoading}) => {
                     <th style={{textAlign:"center"}}>sale Price</th>
                     <th style={{textAlign:"center"}}>Discount</th>
                     <th style={{textAlign:"center"}}>Discount %</th>
-                    <th style={{textAlign:"center"}}>Delete</th>
+                    <th style={{textAlign:"center"}}>   </th>
+                    <th style={{textAlign:"center"}}>   </th>
                 </tr>
             </thead>
           
@@ -76,6 +193,7 @@ const Table = ({setLoader,loader,setLoading}) => {
                             <td>{data[id].data.discount}</td>
                             <td>{data[id].data.discount_percentage}</td>
                             <td><a onClick={()=> onDelete(data[id])}><i className="fa-solid fa-trash"/></a></td>
+                            <td><a ><i class="fa-solid fa-pen-to-square"></i></a></td>
                         </tr>
                     )
                 })
@@ -86,8 +204,9 @@ const Table = ({setLoader,loader,setLoading}) => {
         :
         <div className='noData'>No Data Found</div>:<div className='loader'><ScaleLoader
         size={20}
-        speedMultiplier={2}
+        
       /></div>}
+    </div>
     </div>
   )
 }
@@ -95,6 +214,7 @@ const Table = ({setLoader,loader,setLoading}) => {
 const ProductList = ({setLoader,loader,setLoading,message,setMessage}) =>{
     return(
         <div style={{minHeight:"74vh"}}>
+ 
             <Table setLoader={setLoader} loader={loader} setLoading={setLoading} message={message} setMessage={setMessage} />
         </div>
     )
